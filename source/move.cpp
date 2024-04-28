ef21c5f017fb69d5c54925234a423bfbf94989c0
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-29 00:13:30 +0800

    compile

--- source/move.cpp
+++ source/move.cpp
@@ -2,15 +2,17 @@
 #include <move.h>
 #include <string>
 
+const double StopDistance = 0.2;
+
 string move(Player_T player, double x, double y) {
   string s = "Move ";
-  if (player.x < x - 0.3)
+  if (player.x < x - StopDistance)
     s += "R";
-  if (player.x > x + 0.3)
+  if (player.x > x + StopDistance)
     s += "L";
-  if (player.y > y + 0.3)
+  if (player.y > y + StopDistance)
     s += "U";
-  if (player.y < y - 0.3)
+  if (player.y < y - StopDistance)
     s += "D";
   return s;
 }
