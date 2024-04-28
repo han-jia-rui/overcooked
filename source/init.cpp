955c6486582054666217066aff4fc5531d68b3e0
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-28 21:33:31 +0800

    compile

--- source/init.cpp
+++ source/init.cpp
@@ -122,83 +122,83 @@ bool frame_read(int nowFrame) {
       Order[i].require.push_back(s);
     }
   }
-  // ss >> Player_cnt;
-  // assert(Player_cnt == 2);
-  // /* 读入玩家坐标、x方向速度、y方向速度、剩余复活时间 */
-  // for (int i = 0; i < Player_cnt; i++) {
-  //   ss >> Player[i].x >> Player[i].y >> Player[i].X_Velocity >>
-  //       Player[i].Y_Velocity >> Player[i].live;
-  //   getline(ss, s);
-  //   std::stringstream tmp(s);
-  //   Player[i].containerKind = Container_T::None;
-  //   Player[i].entity.clear();
-  //   while (tmp >> s) {
-  //     /*
-  //         若若该玩家手里有东西，则接下来一个分号，分号后一个空格，空格后为一个实体。
-  //         以下是可能的输入（省略前面的输入）：
-  //          ;  : fish
-  //          ; @  : fish
-  //          ; @ Plate : fish
-  //          ; Plate
-  //          ; DirtyPlates 1
-  //         ...
-  //     */
-  //
-  //     /* 若你不需要处理这些，可直接忽略 */
-  //     if (s == ";" || s == ":" || s == "@" || s == "*")
-  //       continue;
-  //
-  //     /*
-  //         Todo: 其他容器
-  //     */
-  //     if (s == "Plate")
-  //       Player[i].containerKind = Container_T::Plate;
-  //     else if (s == "DirtyPlates")
-  //       Player[i].containerKind = Container_T::DirtyPlates;
-  //     else
-  //       Player[i].entity.push_back(s);
-  //   }
-  // }
-  //
-  // ss >> Entity_cnt;
-  // /* 读入实体坐标 */
-  // for (int i = 0; i < Entity_cnt; i++) {
-  //   ss >> Entity[i].x >> Entity[i].y;
-  //   getline(ss, s);
-  //   std::stringstream tmp(s);
-  //   Entity[i].containerKind = Container_T::None;
-  //   Entity[i].entity.clear();
-  //   Entity[i].currentFrame = Entity[i].totalFrame = 0;
-  //   Entity[i].sum = 1;
-  //   while (tmp >> s) {
-  //     /*
-  //         读入一个实体，例子：
-  //         DirtyPlates 2
-  //         fish
-  //         DirtyPlates 1 ; 15 / 180
-  //
-  //     */
-  //
-  //     /* 若你不需要处理这些，可直接忽略 */
-  //     if (s == ":" || s == "@" || s == "*")
-  //       continue;
-  //     if (s == ";") {
-  //       tmp >> Entity[i].currentFrame >> s >> Entity[i].totalFrame;
-  //       assert(s == "/");
-  //       break;
-  //     }
-  //
-  //     /*
-  //         Todo: 其他容器
-  //     */
-  //     if (s == "Plate")
-  //       Entity[i].containerKind = Container_T::Plate;
-  //     else if (s == "DirtyPlates") {
-  //       Entity[i].containerKind = Container_T::DirtyPlates;
-  //       tmp >> Entity[i].sum;
-  //     } else
-  //       Entity[i].entity.push_back(s);
-  //   }
-  // }
+  ss >> Player_cnt;
+  assert(Player_cnt == 2);
+  /* 读入玩家坐标、x方向速度、y方向速度、剩余复活时间 */
+  for (int i = 0; i < Player_cnt; i++) {
+    ss >> Player[i].x >> Player[i].y >> Player[i].X_Velocity >>
+        Player[i].Y_Velocity >> Player[i].live;
+    getline(ss, s);
+    std::stringstream tmp(s);
+    Player[i].containerKind = Container_T::None;
+    Player[i].entity.clear();
+    while (tmp >> s) {
+      /*
+          若若该玩家手里有东西，则接下来一个分号，分号后一个空格，空格后为一个实体。
+          以下是可能的输入（省略前面的输入）：
+           ;  : fish
+           ; @  : fish
+           ; @ Plate : fish
+           ; Plate
+           ; DirtyPlates 1
+          ...
+      */
+
+      /* 若你不需要处理这些，可直接忽略 */
+      if (s == ";" || s == ":" || s == "@" || s == "*")
+        continue;
+
+      /*
+          Todo: 其他容器
+      */
+      if (s == "Plate")
+        Player[i].containerKind = Container_T::Plate;
+      else if (s == "DirtyPlates")
+        Player[i].containerKind = Container_T::DirtyPlates;
+      else
+        Player[i].entity.push_back(s);
+    }
+  }
+
+  ss >> Entity_cnt;
+  /* 读入实体坐标 */
+  for (int i = 0; i < Entity_cnt; i++) {
+    ss >> Entity[i].x >> Entity[i].y;
+    getline(ss, s);
+    std::stringstream tmp(s);
+    Entity[i].containerKind = Container_T::None;
+    Entity[i].entity.clear();
+    Entity[i].currentFrame = Entity[i].totalFrame = 0;
+    Entity[i].sum = 1;
+    while (tmp >> s) {
+      /*
+          读入一个实体，例子：
+          DirtyPlates 2
+          fish
+          DirtyPlates 1 ; 15 / 180
+
+      */
+
+      /* 若你不需要处理这些，可直接忽略 */
+      if (s == ":" || s == "@" || s == "*")
+        continue;
+      if (s == ";") {
+        tmp >> Entity[i].currentFrame >> s >> Entity[i].totalFrame;
+        assert(s == "/");
+        break;
+      }
+
+      /*
+          Todo: 其他容器
+      */
+      if (s == "Plate")
+        Entity[i].containerKind = Container_T::Plate;
+      else if (s == "DirtyPlates") {
+        Entity[i].containerKind = Container_T::DirtyPlates;
+        tmp >> Entity[i].sum;
+      } else
+        Entity[i].entity.push_back(s);
+    }
+  }
   return false;
 }
