4a4ee1af022cbf09b6de9666484af75e835a39f5
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-29 00:11:47 +0800

    compile

--- source/move.cpp
+++ source/move.cpp
@@ -9,8 +9,8 @@ string move(Player_T player, double x, double y) {
   if (player.x > x + 0.3)
     s += "L";
   if (player.y > y + 0.3)
-    s += "D";
-  if (player.y < y - 0.3)
     s += "U";
+  if (player.y < y - 0.3)
+    s += "D";
   return s;
 }
