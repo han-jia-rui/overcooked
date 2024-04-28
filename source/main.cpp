704a1aea66d136fcfe78a31398cce938c39082a9
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-28 21:48:25 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -16,8 +16,8 @@ int main() {
 
     /* 输出当前帧的操作，此处仅作示例 */
     cout << "Frame " << i << "\n";
-    string player0_Action = "Move DD";
-    string player1_Action = "Move RR";
+    string player0_Action = "Move DDDD";
+    string player1_Action = "Move DDDD";
 
     /* 合成一个字符串再输出，否则输出有可能会被打断 */
     string action = player0_Action + "\n" + player1_Action + "\n";
