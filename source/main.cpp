5032876c41c89b3b023c1d9cd9283a9d89f4eda6
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-28 21:30:34 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -1,37 +1,37 @@
+#include <init.h>
 #include <iostream>
 #include <string>
-#include <init.h>
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cerr.tie(nullptr);
-    cerr << std::nounitbuf;
-    int frame;
+int main() {
+  ios::sync_with_stdio(false);
+  cerr.tie(nullptr);
+  cerr << std::nounitbuf;
+  int frame;
 
-    init_read();
+  init_read();
 
-    /*
-        你可以在读入后进行一些相关预处理，时间限制：5秒钟
-        init();
-    */
+  /*
+      你可以在读入后进行一些相关预处理，时间限制：5秒钟
+      init();
+  */
+  cerr << "HERE" << endl;
 
-    int totalFrame = 14400;
-    for (int i = 0; i < totalFrame; i++)
-    {
-        bool skip = frame_read(i);
-        if (skip) continue;
+  int totalFrame = 14400;
+  for (int i = 0; i < totalFrame; i++) {
+    bool skip = frame_read(i);
+    if (skip)
+      continue;
 
-        /* 输出当前帧的操作，此处仅作示例 */
-        cout << "Frame " << i << "\n";
-        string player0_Action = "Move R";
-        string player1_Action = "Move U";
+    /* 输出当前帧的操作，此处仅作示例 */
+    cout << "Frame " << i << "\n";
+    string player0_Action = "Move R";
+    string player1_Action = "Move U";
 
-        /* 合成一个字符串再输出，否则输出有可能会被打断 */
-        string action = player0_Action + "\n" + player1_Action + "\n";
-        cout << action;
+    /* 合成一个字符串再输出，否则输出有可能会被打断 */
+    string action = player0_Action + "\n" + player1_Action + "\n";
+    cout << action;
 
-        /* 不要忘记刷新输出流，否则游戏将无法及时收到响应 */
-        cout.flush();
-    }
+    /* 不要忘记刷新输出流，否则游戏将无法及时收到响应 */
+    cout.flush();
+  }
 }
