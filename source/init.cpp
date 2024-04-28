2afe1ea7708dd8b7b783675e1f8aa3439fdc3f00
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-28 20:13:22 +0800

    compile

--- source/init.cpp
+++ source/init.cpp
@@ -33,51 +33,51 @@ void init_read() {
       ss >> Map.map[i][j];
 
   /* 读入原料箱：位置、名字、以及采购单价 */
-  ss >> Ingredient_cnt;
-  Ingredient.resize(Ingredient_cnt);
-  for (int i = 0; i < Ingredient_cnt; i++) {
-    ss >> s;
-    assert(s == "IngredientBox");
-    ss >> Ingredient[i].x >> Ingredient[i].y >> Ingredient[i].name >>
-        Ingredient[i].price;
-  }
-
-  /* 读入配方：加工时间、加工前的字符串表示、加工容器、加工后的字符串表示 */
-  ss >> Recipe_cnt;
-  Recipe.reserve(Recipe_cnt);
-  for (int i = 0; i < Recipe_cnt; i++) {
-    ss >> Recipe[i].time >> Recipe[i].nameBefore >> Recipe[i].operation >>
-        Recipe[i].nameAfter;
-  }
-
-  /* 读入总帧数、当前采用的随机种子、一共可能出现的订单数量 */
-  ss >> totalTime >> randomizeSeed >> totalOrderCount;
-
-  /* 读入订单的有效帧数、价格、权重、订单组成 */
-  for (int i = 0; i < totalOrderCount; i++) {
-    ss >> totalOrder[i].validFrame >> totalOrder[i].price >>
-        totalOrder[i].frequency;
-    getline(ss, s);
-    std::stringstream tmp(s);
-    while (tmp >> s)
-      totalOrder[i].recipe.push_back(s);
-  }
-
-  /* 读入玩家信息：初始坐标 */
-  ss >> k;
-  assert(k == 2);
-  for (int i = 0; i < k; i++) {
-    ss >> Players[i].x >> Players[i].y;
-    Players[i].containerKind = Container_T::None;
-    Players[i].entity.clear();
-  }
-
-  /* 读入实体信息：坐标、实体组成 */
-  ss >> entityCount;
-  for (int i = 0; i < entityCount; i++) {
-    ss >> Entity[i].x >> Entity[i].y >> s;
-    Entity[i].entity.push_back(s);
-  }
+  // ss >> Ingredient_cnt;
+  // Ingredient.resize(Ingredient_cnt);
+  // for (int i = 0; i < Ingredient_cnt; i++) {
+  //   ss >> s;
+  //   assert(s == "IngredientBox");
+  //   ss >> Ingredient[i].x >> Ingredient[i].y >> Ingredient[i].name >>
+  //       Ingredient[i].price;
+  // }
+  //
+  // /* 读入配方：加工时间、加工前的字符串表示、加工容器、加工后的字符串表示 */
+  // ss >> Recipe_cnt;
+  // Recipe.reserve(Recipe_cnt);
+  // for (int i = 0; i < Recipe_cnt; i++) {
+  //   ss >> Recipe[i].time >> Recipe[i].nameBefore >> Recipe[i].operation >>
+  //       Recipe[i].nameAfter;
+  // }
+  //
+  // /* 读入总帧数、当前采用的随机种子、一共可能出现的订单数量 */
+  // ss >> totalTime >> randomizeSeed >> totalOrderCount;
+  //
+  // /* 读入订单的有效帧数、价格、权重、订单组成 */
+  // for (int i = 0; i < totalOrderCount; i++) {
+  //   ss >> totalOrder[i].validFrame >> totalOrder[i].price >>
+  //       totalOrder[i].frequency;
+  //   getline(ss, s);
+  //   std::stringstream tmp(s);
+  //   while (tmp >> s)
+  //     totalOrder[i].recipe.push_back(s);
+  // }
+  //
+  // /* 读入玩家信息：初始坐标 */
+  // ss >> k;
+  // assert(k == 2);
+  // for (int i = 0; i < k; i++) {
+  //   ss >> Players[i].x >> Players[i].y;
+  //   Players[i].containerKind = Container_T::None;
+  //   Players[i].entity.clear();
+  // }
+  //
+  // /* 读入实体信息：坐标、实体组成 */
+  // ss >> entityCount;
+  // for (int i = 0; i < entityCount; i++) {
+  //   ss >> Entity[i].x >> Entity[i].y >> s;
+  //   Entity[i].entity.push_back(s);
+  // }
 }
 
 bool frame_read(int nowFrame) {
