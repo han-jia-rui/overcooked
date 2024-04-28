b8e42393be57f44f225b37f0061f5d4ee7338031
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-28 19:43:02 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -18,7 +18,7 @@ int main()
         你可以在读入后进行一些相关预处理，时间限制：5秒钟
         init();
     */
-  cerr << "HERE\n";
+  cerr << "HERE\n" << flush;
 
     int totalFrame = 14400;
     for (int i = 0; i < totalFrame; i++)
