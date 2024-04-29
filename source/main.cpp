9f869441782138fa63a9cb0d72f8d00630885135
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-29 11:40:49 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,8 @@
+#include <common.h>
 #include <frame.h>
 #include <init.h>
-#include <motion.h>
 #include <iostream>
-#include <common.h>
+#include <motion.h>
 
 int main() {
   ios::sync_with_stdio(false);
@@ -18,11 +18,22 @@ int main() {
     cout << "Frame " << i << "\n";
     Player[0].action = "";
     Player[1].action = "";
-    if(Player[0].entity.name.empty())
-    Pick(Player[0], Ingredient[0].coord);
+    if (Player[0].entity.container == Container_T::Plate &&
+        Player[0].entity.name.size() > 1)
+      Put(Player[0], ServiceWindow.coord);
     else {
-      for(auto entity : Entity){
-        if(entity.container == Container_T::Plate && entity.name.size() == 1){
+      for (auto entity : Entity) {
+        if (entity.container == Container_T::Plate && entity.name.size() > 1) {
+          Pick(Player[0], entity.coord);
+          break;
+        }
+      }
+    }
+    if (Player[0].action == "" && Player[0].entity.name.empty())
+      Pick(Player[0], Ingredient[0].coord);
+    else {
+      for (auto entity : Entity) {
+        if (entity.container == Container_T::Plate && entity.name.size() == 1) {
           Put(Player[0], entity.coord);
           break;
         }
