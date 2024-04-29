ee3da81ce37de29a5d13fb493ecaa914c02ebe26
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-04-29 11:03:04 +0800

    compile

--- source/motion.cpp
+++ source/motion.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <motion.h>
 
-const double StopDistance = 0.4;
+const double StopDistance = 0.3;
 const double InteractDistance = 1.2;
 
 double dist(double x1, double y1, double x2, double y2) {
