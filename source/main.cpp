3e5aa05ed6e5452713106d0a42387b9a40bfc6f0
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-05-14 00:32:54 +0800

    compile

--- source/main.cpp
+++ source/main.cpp
@@ -16,13 +16,13 @@ int main() {
 
     /* 输出当前帧的操作，此处仅作示例 */
     cout << "Frame " << i << "\n";
-    if (Player[0].live == 0)
-      Player[0].action.clear();
-    if (Player[1].live == 0)
-      Player[1].action.clear();
+    Player[0].action.clear();
+    Player[1].action.clear();
 
-    prepareOrder(Player[0], Order[0]);
-    washPlate(Player[1]);
+    if (Player[0].live == 0)
+      prepareOrder(Player[0], Order[0]);
+    if (Player[1].live == 0)
+      washPlate(Player[1]);
 
     /* 合成一个字符串再输出，否则输出有可能会被打断 */
     string action =
