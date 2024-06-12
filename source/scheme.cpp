b7e10b692a8ef9d85511c4410cab4f096cac7e11
tracer-ps4 <tracer-ps4@nju.edu.cn>
2024-06-12 21:01:14 +0800

    compile

--- source/scheme.cpp
+++ source/scheme.cpp
@@ -1,5 +1,10 @@
+#include "action.h"
+#include "common.h"
+#include "enum.h"
+#include "task.h"
 #include <iostream>
 #include <scheme.h>
+#include <vector>
 
 void Scheme1(Player_T &player) {
   CheckAlive;
@@ -22,6 +27,7 @@ void Scheme1(Player_T &player) {
       if (!entity.fixed && entity.checkOrder(order)) {
         flag = true;
         entity.fixed = true;
+        break;
       }
     }
     if (flag)
@@ -42,38 +48,38 @@ void Scheme1(Player_T &player) {
     }
   }
 
-  // int plate_cnt = 0;
-  // for (auto entity : Entity) {
-  //   if (entity.container == Container_Kind::Plate) {
-  //     for (auto food : Order[plate_cnt].require) {
-  //       if (!entity.findfood(food)) {
-  //         getFood(player, food);
-  //         CheckAction;
-  //         if (player.entity.findfood(food))
-  //           Put(player, entity.coord);
-  //         CheckAction;
-  //       }
-  //     }
-  //     break;
-  //   }
-  // }
   CheckAction;
 }
 
 void Scheme2(Player_T &player) {
   CheckAlive;
   for (auto order : Order) {
-    service(player, Order[0]);
+    service(player, order);
     CheckAction;
   }
   CheckAction;
   Tile_T Sink = getFirstTile(Tile_Kind::Sink);
+  if (player.entity.is(Container_Kind::Plate)) {
+    std::vector<Tile_T> tmp = getTile(Tile_Kind::Table);
+    for (auto table : tmp) {
+      if (!isTileUsed(table) && table.coord.x != table.coord.y) {
+        Put(player, table.coord);
+        CheckAction;
+      }
+    }
+  }
+  CheckAction;
+  Tile_T PlateRack = getFirstTile(Tile_Kind::PlateRack);
+  if (isTileUsed(PlateRack) && player.entity.is(Container_Kind::None)) {
+    Pick(player, PlateRack.coord);
+  }
+  CheckAction;
   if (player.entity.empty()) {
     for (auto entity : Entity) {
       if (entity.container == Container_Kind::DirtyPlates &&
           entity.coord.x == Sink.coord.x && Sink.coord.y == entity.coord.y) {
         Interact(player, entity.coord);
-        break;
+        CheckAction;
       }
     }
   }
@@ -82,7 +88,7 @@ void Scheme2(Player_T &player) {
     for (auto entity : Entity) {
       if (entity.container == Container_Kind::DirtyPlates) {
         Pick(player, entity.coord);
-        break;
+        CheckAction;
       }
     }
   }
@@ -92,5 +98,4 @@ void Scheme2(Player_T &player) {
     Put(player, Sink.coord);
   }
   CheckAction;
-  gotoTile(player, Tile_Kind::PlateReturn);
 }
